2f65b46c87e2b706a102175be06ec40166b6618c
natefduncan <natefduncan@gmail.coM>
2021-06-25 20:46:39 -0500

    offset

--- src/grid.rs
+++ src/grid.rs
@@ -1,10 +1,42 @@
 //Lat = Y Long = X
-use geo::{Point};
+use geo::{Point, Rect};
 use geo::algorithm::geodesic_distance::GeodesicDistance; 
 
+pub enum Offset {
+    North, 
+    South,
+    East, 
+    West
+}
+
 pub fn miles(p1 : &Point<f64>, p2 : &Point<f64>) -> f64 {
-    let meters_to_miles : f64 = 0.00062137119223733;
+    let meters_to_miles : f64 = 0.00062137119223733; 
     let meters = p1.geodesic_distance(&p2); 
     let miles : f64 = meters_to_miles * meters; 
     return miles
 }
+
+pub fn get_offset(start_point : &Point<f64>, miles : f64, offset : Offset) -> Point<f64> {
+    let miles_to_degrees : f64 = 64.52626802; 
+    let degrees = miles / miles_to_degrees; 
+    let mut lat_translate : f64 = 0. ; 
+    let mut lng_translate : f64 = 0. ; 
+    match offset {
+        Offset::North => {lat_translate = lat_translate + degrees} 
+        Offset::South => {lat_translate = lat_translate - degrees;}
+        Offset::East => {lng_translate = lng_translate + degrees;}
+        Offset::West => {lng_translate = lat_translate - degrees;}
+    }
+    let output : Point<f64> = (start_point.lng() + lng_translate, start_point.lat() + lat_translate).into();
+    return output
+}
+
+/*
+pub fn get_rect(center_point : &Point<f64>, length_miles : f64) -> Rect<f64> {
+    center_point.lat(); 
+    center_point.lng(); 
+}*/
+
+//1. Select start point. 
+//2. Select distance from center. 
+//3. Generate boxes.
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use geo::{Point};
 
 
 fn main() {
+    //url = f"https://duckduckgo.com/local.js?q={quote(q)}&tg=maps_places&rt=D&mkexp=b&is_requery=1&bbox_tl={','.join([str(i) for i in box_tl])}&bbox_br={','.join([str(i) for i in box_br])}&strict_bbox=1&wiamr=a&nyexp=b"
     let dallas: Point<f64> = (-96.80667, 32.78306).into();
-    let austin: Point<f64> = (-97.74306, 30.26715).into();
-    println!("Miles: {}", grid::miles(&dallas, &austin)); 
+    println!("From {:?} to {:?}", &dallas, grid::get_offset(&dallas, 10., grid::Offset::North)); 
 }
+
+
