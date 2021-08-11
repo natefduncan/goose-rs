65760d44dee95645f83b1ee7381f819e301afc9d
Nathan Duncan <natefduncan@gmail.com>
2021-08-11 09:06:32 -0500

    warning cleanup and support for multiple queries and search

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 extern crate clap;
 use clap::{App, Arg};
-use std::time::Instant;
-use console::Term; 
 
 mod ddg;
 mod files;
@@ -15,7 +13,7 @@ fn main() {
         .about("Query Duck Duck Go to get location data.")
         .arg(
             Arg::with_name("QUERY")
-                .help("Sets your search value (e.g. Restaurant, Park, etc).")
+                .help("Sets your search value (e.g. Restaurant, Park, etc). Multiple queries in the same location can be separated by comma.")
                 .required(true)
                 .index(1),
         )
@@ -24,12 +22,6 @@ fn main() {
                 .help("Sets your location (e.g. Dallas, TX).")
                 .required(true)
                 .index(2),
-        )
-        .arg(
-            Arg::with_name("SAVE")
-                .help("Sets your save location. Default is current directory.")
-                .required(false)
-                .index(3),
         )
         .arg(
             Arg::with_name("DISTANCE")
@@ -58,29 +50,32 @@ fn main() {
         .get_matches();
 
     let query = matches.value_of("QUERY").expect("Unable to parse query");
+    let queries : Vec<&str>; 
+    if query.contains(",") {
+        queries = query.split(",").collect::<Vec<&str>>(); 
+    } else {
+        queries = vec![query];
+    }
     let location = matches
         .value_of("LOCATION")
         .expect("Unable to parse location");
-    let save = matches.value_of("SAVE").unwrap_or(".");
     let distance = matches.value_of("DISTANCE").unwrap_or("10");
     let file_type = matches.value_of("FILE-TYPE").unwrap_or("json");
     let concurrent_requests = matches.value_of("CONCURRENCY").unwrap_or("1");
     let start_point = geocode::geocode(&location);
-    /*println!(
-        "Found coordinates for {}: {}, {}",
-        location,
-        start_point.lat(),
-        start_point.lng()
-    );*/
-    //println!("Searching for {} within {} miles.", query, distance);
-    let now = Instant::now();
-    let data: Vec<ddg::Place> = ddg::query(
-        &query,
-        &start_point,
-        distance.parse::<f64>().unwrap(),
-        concurrent_requests.parse::<usize>().unwrap(),
-    );
-    //println!("Completed query in {} second.", now.elapsed().as_secs());
+    let mut data: Vec<ddg::Place> = vec![];
+    for query in queries {
+        let temp = ddg::query(
+            &query,
+            &start_point,
+            distance.parse::<f64>().unwrap(),
+            concurrent_requests.parse::<usize>().unwrap(),
+        );
+        for mut t in temp {
+            t.search = Some(query.to_string()); 
+            data.push(t); 
+        }
+    }
     if file_type == "csv" {
         files::output_as_csv(data).expect("Write to csv failed.");
     } else if file_type == "json" {
